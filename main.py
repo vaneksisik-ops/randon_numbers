9f1853933dba4384a6d5cb84f82b2831a7bd6846
vaneksisik-ops <vaneksisik@gmail.com>
2026-03-04 21:13:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import random
 from time import sleep
 num1 = int(input("введите число от которого будет начинатся отчёт: "))
 num2 = int(input("введите число которым будет заканчиватсья отчёт: "))
-while True:
-    random_number = random.randint(num1, num2)
-    print(random_number)
-    sleep(0.1)
+random_number = random.randint(num1, num2)
+print(random_number)
+print("рандомное число сгенирировалось")
+sleep(0.1)
+number_of_users = int(input("введите ваше число"))
+if number_of_users == random_number:
+    print("вы угадали!")
