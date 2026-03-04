b101a13691561650f2831663b7711e69e7285496
vaneksisik-ops <vaneksisik@gmail.com>
2026-03-04 21:35:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import random
 from time import sleep
-num1 = int(input("введите число от которого будет начинатся отчёт: "))
-num2 = int(input("введите число которым будет заканчиватсья отчёт: "))
-random_number = random.randint(num1, num2)
 
-print("рандомное число сгенирировалось")
 while True:
-    number_of_users = int(input("введите ваше число"))
-    if number_of_users == random_number:
-      print("вы угадали!")
-    elif number_of_users > random_number:
-       print("ваше  число больше")
-    else:
-      print("ваше число меньше")
+    num1 = int(input("введите число от которого будет начинаться отчёт: "))
+    num2 = int(input("введите число которым будет заканчиваться отчёт: "))
+    random_number = random.randint(num1, num2)
+
+    print("рандомное число сгенерировалось")
+
+    while True:
+        number_of_users = int(input("введите ваше число: "))
+        if number_of_users == random_number:
+            print("вы угадали!")
+            break 
+        elif number_of_users > random_number:
+            print("ваше число больше")
+        else:
+            print("ваше число меньше")
